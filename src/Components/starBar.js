f40792bddab82b3e1b5f3851ee7362f6f8fe1e06
osman122 <osoconan1@gmail.com>
2023-10-01 16:42:34 +0300

    add star bar

---
+++ src/Components/starBar.js
@@ -0,0 +1,22 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar as fullStar, faStarHalfStroke } from '@fortawesome/free-solid-svg-icons'
+import { faStar as emptyStar } from '@fortawesome/free-regular-svg-icons'
+
+export default function Stars (props) {
+    let {rating} = props
+
+const stars = []
+    for (let x=0; x < 5; x++){
+        if (rating >= 1) {
+            stars.push(<FontAwesomeIcon icon={fullStar} key={x} style={{color:"#28bbbd"}} className='color-info'/>)
+            rating -= 1
+        } else if ( 0 < rating ) {
+            stars.push(<FontAwesomeIcon icon={faStarHalfStroke} key={x} style={{color:"#28bbbd"}}/>)
+            rating = 0
+        } else {
+            stars.push(<FontAwesomeIcon icon={emptyStar} key={x} style={{color:"#28bbbd"}}/>)
+        }
+    }
+
+    return <div className="rating">{stars}</div>
+}
--- src/pages/productDetails.js
+++ src/pages/productDetails.js
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom"
 import { axiosInstance } from "../apis/config";
 import { useEffect, useState } from 'react'
-
+import StarsBar from "../Components/starBar";
 import {Badge} from 'react-bootstrap';
 
 
@@ -24,7 +24,7 @@ export default function ProductDetails() {
                 <img style={{maxHeight:"450px " ,objectFit:"cover"}} className="w-100 h-100 " src={productData.thumbnail} alt=""></img>
                 <div className="" style={{display:"flex", height:"100px",width:"100% ", justifyContent:"space-around", border:"2px solid #ccc"}} >
                     {productData.images.slice(0,3).map((imageURL, index)=>{
-                        return <img className="product-img img-fluid p-1" style={{objectFit:"cover"}} src={imageURL} key={index} alt="Product"/> 
+                        return <img className="img-fluid p-1" style={{objectFit:"cover"}} src={imageURL} key={index} alt="Product"/> 
                     })}
                 </div>
             </div>
@@ -38,6 +38,7 @@ export default function ProductDetails() {
                 <div className="my-5">
                     
                     <h2 className={productData.discountPercentage? 'd-inline-block text-decoration-line-through':''} >$ {productData.price}</h2>
+                    
                     {productData.discountPercentage?
                     
                         <><h2 className="fst-italic d-inline-block mt-2 ms-3">${((100 - productData.discountPercentage) * productData.price / 100).toFixed(1)}</h2>
@@ -51,8 +52,8 @@ export default function ProductDetails() {
                         :<Badge bg="danger">Out of Stock</Badge>}
                         <p className="m-0 d-flex align-items-center"><strong>Brand:</strong>{productData.brand}</p>
                     </div>
-                    
-                    
+                    <hr/>
+                    <StarsBar rating={productData.rating}/>
                 </div>
                 <button style={{width:"80%"}} type="button" class="btn btn-info ">ADD TO CART</button>
             </div>
